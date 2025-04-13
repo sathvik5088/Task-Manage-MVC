ab3b9e9c24265fd514584d6f763ac533bd814d08
sathvik5088 <ksathvik351@gmail.com>
2025-04-13 12:11:15 +0530

    V2

--- obj/Debug/net8.0/TaskManagerAPP.AssemblyInfo.cs
+++ obj/Debug/net8.0/TaskManagerAPP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagerAPP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a613caf5baf8e4e9f3c65e007121b5958d9a182")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagerAPP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagerAPP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
